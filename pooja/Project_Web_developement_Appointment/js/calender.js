4e82aefa9b287adf7ea32e50d8be3331455bfd2d
poojakamble <kamblepooja24.pk@gmail.com>
2020-05-11 23:32:53 +0530

    project folder added:Project_web_developement

---
+++ pooja/Project_Web_developement_Appointment/js/calender.js
@@ -0,0 +1,68 @@
+var dt = new Date();
+
+function renderDate() {
+
+  dt.setDate(1);
+  var day = dt.getDay();
+  var today = new Date();
+  var endDate = new Date(
+    dt.getFullYear(),
+    dt.getMonth() + 1,
+    0
+  ).getDate();
+
+  var prevDate = new Date(
+    dt.getFullYear(),
+    dt.getMonth(),
+    0
+  ).getDate();
+  var months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ]
+  console.log(dt.day);
+  document.getElementById("month").innerHTML = months[dt.getMonth()];
+  document.getElementById("date_str").innerHTML = dt.toDateString();
+  var cells = "";
+  for (x = day; x > 0; x--) {
+    cells += "<div class='prev_date'>" + (prevDate - x + 1) + "</div>";
+  }
+  console.log(day);
+  for (i = 1; i <= endDate; i++) {
+    if (i == today.getDate() && dt.getMonth() == today.getMonth()) cells += "<div class='today'>" + i + "</div>";
+    else
+      cells += "<div>" + i + "</div>";
+  }
+  document.getElementsByClassName("days")[0].innerHTML = cells;
+
+}
+
+function moveDate(para) {
+  if (para == "prev") {
+    dt.setMonth(dt.getMonth() - 1);
+  } else if (para == 'next') {
+    dt.setMonth(dt.getMonth() + 1);
+  }
+  renderDate();
+}
+
+
+function open_model() {
+  document.getElementById('id02').style.display = 'block';
+  document.getElementById('id01').style.display = 'none';
+}
+
+function open_model_register() {
+  document.getElementById('id01').style.display = 'block';
+  document.getElementById('id02').style.display = 'none';
+}
