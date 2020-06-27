12150b24774916fac1d9f56b4cb431a55596b4b0
shubhali-github <shubhaliharode112@gmail.com>
2020-06-27 12:39:03 +0530

    update files as per coding rules

--- shubhali/curdop/modules/employee.js
+++ shubhali/curdop/modules/employee.js
@@ -1,13 +1,13 @@
-const mongoose=require('mongoose')
-mongoose.connect('mongodb://localhost:27017/mydb',{useNewUrlParser:true});
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/mydb', { useNewUrlParser: true});
 mongoose.Collection;
-var employeeSchema =new mongoose.Schema({
+var employeeSchema = new mongoose.Schema({
  name: String,
- lastname:String,
+ lastname: String,
  email: String,
  phone: Number,
  field: String,
  city: String,
 });
 var employeeModel = mongoose.model('Employee', employeeSchema);
-module.exports=employeeModel;
+module.exports = employeeModel;
