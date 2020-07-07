f9ed070520dccc3ca378715dd1e2c70251cc26ca
shubhali-github <shubhaliharode112@gmail.com>
2020-07-07 19:33:05 +0530

    Updated package.json

--- shubhali/curdop/app.js
+++ shubhali/curdop/app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var app = express();
@@ -8,7 +7,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use(function(req, res, next) {
