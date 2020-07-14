7cbd30ff42c78e3bd9ec20377f77f21911b44a9e
shubhali-github <63222821+shubhali-github@users.noreply.github.com>
2020-07-14 11:25:14 +0530

    Files added: curdop nodejs project  (#103)
    * Added assignment CURD Operation file

---
+++ shubhali/curdop/routes/index.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var router = express.Router();
+var empModel = require('../modules/employee');
+var employee = empModel.find({});
+router.get('/', function(req, res, next) {
+  employee.exec(function(err, data) {
+    if(err) throw err;
+    res.render('index', { title: 'Employee Records', records: data });
+  })
+});
+router.post('/', function(req, res, next) {
+  var empDetails = new empModel({
+    name: req.body.uname,
+    email: req.body.email,
+    lastname: req.body.lastname,
+    phone: req.body.phone,
+    field: req.body.field,
+    city: req.body.city,
+  });
+  empDetails.save(function(err, req1) {
+    if(err) throw err;
+    employee.exec(function(err, data) {
+      if(err) throw err;
+      res.render('index', { title: 'Employee Records', records: data, success: 'Record Inserted Successfully' });
+    });
+  })
+});
+router.get('/delete/:id', function(req, res, next){
+  var id = req.params.id;
+  var del = empModel.findByIdAndDelete(id);
+  del.exec(function(err){
+    if(err) throw err;
+    res.redirect('/');
+  });
+});
+router.get('/edit/:id', function(req, res, next) {
+  var id = req.params.id;
+  var updatevalue = empModel.findById(id);
+  updatevalue.exec(function(err,data) {
+    if(err) throw err;
+    res.render('edit', { title: ' Edit employee Records', records: data, success: 'Record Inserted Successfully' });
+  });
+})
+router.post('/update/',function(req, res, next) {
+  var update = empModel.findByIdAndUpdate(req.body.id, {
+    name: req.body.uname,
+    email: req.body.email,
+    lastname: req.body.lastname,
+    phone: req.body.phone,
+    field: req.body.field,
+    city: req.body.city,
+  });
+  update.exec(function(err, data) {
+    if(err) throw err;
+    res.redirect('/');
+  });
+})
+module.exports = router;
