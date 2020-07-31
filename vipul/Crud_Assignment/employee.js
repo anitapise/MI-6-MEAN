3df0bd8fc4ea27bb2763faada4612e79d195b164
VIPUL2812 <vipulsuryawanshi28@gmail.com>
2020-07-31 10:26:44 +0530

    Files Added : Crud Operations Assignment

--- vipul/Crud_Assignment/employee.js
+++ vipul/Crud_Assignment/employee.js
@@ -1,21 +1,22 @@
 var express =require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+
 // make a connection
 mongoose.connect('mongodb://localhost:27017/Employee', {useNewUrlParser: true,useUnifiedTopology: true, useFindAndModify: false});
+
 // get reference to database
 var db = mongoose.connection;
-
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use(bodyParser.json());
-
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
     console.log("Connection Successful!");
 });
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
 // define Schema
 var employeeSchema = mongoose.Schema({
@@ -30,21 +31,23 @@ var employeeModel = mongoose.model('Employee', employeeSchema);
 
 // set ejs as templating engine
 app.set('view engine', 'ejs');
+app.use('/public', express.static('public'));
 
 app.get('/', function(req, res) {
   employeeModel.find({}, function (err, docs) {
-    if (!err) {
-      // console.log(docs);
-      res.render('employee', {values:docs});
+    if (err) {
+      console.log(err);
     } else {
-      onErr(err, callback);
+      res.render('employee', {values:docs});
     }
   });
 });
+
 // add details
 app.get('/addEmployee', function(req, res) {
   res.render('addEmployee');
  });
+
 app.post('/add', function(req, res, next) {
     var employModel = new employeeModel({
       firstName: req.body.fname,
@@ -56,13 +59,12 @@ app.post('/add', function(req, res, next) {
   console.log(employeeModel);
   // save records to the collection 
     employModel.save(function (err, employModel) {
-    if (err) return console.error(err); 
+    if (err) return console.log(err); 
     employeeModel.find({}, function (err, docs) {
-      if (!err) {
-        // console.log(docs);
-        res.render('employee', {values:docs});
+      if (err) {
+        console.log(err);
       } else {
-        onErr(err, callback);
+        res.render('employee', {values:docs});
       }
     });
   });
@@ -71,10 +73,12 @@ app.post('/add', function(req, res, next) {
 //edit details
 app.get('/editEmployee/:id', (req, res) => {
   employeeModel.findById(req.params.id, (err, doc) => {
-      if (!err) {
-          res.render("editEmployee", {
-              values: doc
-          });
+    if (err) {
+      console.log(err);
+    } else {
+        res.render("editEmployee", {
+          values: doc
+        });
       }
   });
 });
@@ -92,15 +96,19 @@ employeeModel.findOneAndUpdate(
     },{ new: true },    
 ).then(result => {
   res.writeHead(302, { "Location": "http://localhost:8080/"});
-      return res.end();
+  // 302 status code indicates that the resource requested has been temporarily moved to the URL given by the Location header
+  return res.end();
   });
 });
 
 app.get('/delete/:id', function(req, res)  {
   employeeModel.findByIdAndRemove(req.params.id, function (err, docs) {
-    if (!err) {
+    if (err) {
+      console.log(err);
+    } else {
       console.log(docs);
       res.writeHead(302, { "Location": "http://localhost:8080/"});
+      // 302 status code indicates that the resource requested has been temporarily moved to the URL given by the Location header
       return res.end();
     } 
   });
@@ -108,4 +116,4 @@ app.get('/delete/:id', function(req, res)  {
 
 app.listen(8080,function(){
     console.log('Listening on port 8080...' );
-  });
+});
